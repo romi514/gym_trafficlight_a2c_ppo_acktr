3dc9a06fc5b4f19c770f190e7f1f319c10242e45
romi514 <romain.leteurtre@gmail.com>
2019-01-30 21:02:03 -0500

    ro

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -166,7 +166,7 @@ class CNNBase(NNBase):
     def __init__(self, occ_num_inputs, sign_num_inputs, recurrent):
 
         combined_size = 4*16*5 + sign_num_inputs
-        hidden_size = int(np.floor(np.power(2,np.floor(np.log2(combined_size))))) # 512
+        hidden_size = np.power(2,np.floor(np.log2(combined_size)))
         
         super(CNNBase, self).__init__(recurrent, hidden_size, hidden_size)
 
