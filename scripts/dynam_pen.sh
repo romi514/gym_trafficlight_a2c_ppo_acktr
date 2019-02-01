8d33417b425eefc665d21c161f4a85b04cdc7bdf
romi514 <romain.leteurtre@gmail.com>
2019-01-31 22:37:34 -0500

    ...

--- scripts/dynam_pen.sh
+++ scripts/dynam_pen.sh
@@ -2,12 +2,12 @@
 
 cd ..
 
-python3 main.py --state-rep original --env-name TrafficLight-simple-medium-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1 --state-rep full
+python3 main.py --env-name TrafficLight-simple-medium-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1 --state-rep full
 wait
-python3 main.py --state-rep original --env-name TrafficLight-Lust12408-regular-time-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1 --state-rep full
+python3 main.py --env-name TrafficLight-Lust12408-regular-time-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1 --state-rep full
 wait
 
-python3 main.py --state-rep original --env-name TrafficLight-simple-medium-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 0.1 --state-rep full
+python3 main.py --env-name TrafficLight-simple-medium-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 0.1 --state-rep full
 wait
-python3 main.py --state-rep original --env-name TrafficLight-Lust12408-regular-time-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 0.1 --state-rep full
+python3 main.py --env-name TrafficLight-Lust12408-regular-time-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 0.1 --state-rep full
 wait
