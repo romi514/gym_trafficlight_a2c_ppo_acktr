dd2079c0d6f3b19650d7f8f049e59123e7e05dd0
romi514 <romain.leteurtre@gmail.com>
2019-02-02 03:50:40 -0500

    new scripts

--- scripts/conv_rate2.sh
+++ scripts/conv_rate2.sh
@@ -2,13 +2,35 @@
 
 cd ..
 
+python3 main.py --state-rep original --env-name TrafficLight-simple-sparse-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-simple-dense-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-midnight-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-rush-hour-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-regular-time-v0 --algo acktr --num-processes 16 --num-steps 32 --use-linear-lr-decay --penetration-rate 0.1
+
 #7h30
-python3 main.py --state-rep original --env-name TrafficLight-simple-sparse-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 1
+python3 main.py --state-rep original --env-name TrafficLight-simple-sparse-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 0.1
 wait
-python3 main.py --state-rep original --env-name TrafficLight-simple-dense-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 1
+python3 main.py --state-rep original --env-name TrafficLight-simple-dense-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 0.1
 wait
-python3 main.py --state-rep original --env-name TrafficLight-Lust12408-midnight-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 1
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-midnight-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 0.1
 wait
-python3 main.py --state-rep original --env-name TrafficLight-Lust12408-rush-hour-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 30000000 --penetration-rate 1
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-rush-hour-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 0.1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-regular-time-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 0.1
 wait
 
+#7h30
+python3 main.py --state-rep original --env-name TrafficLight-simple-sparse-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-simple-dense-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-midnight-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-rush-hour-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 1
+wait
+python3 main.py --state-rep original --env-name TrafficLight-Lust12408-regular-time-v0 --algo a2c --num-processes 10 --num-steps 16 --use-linear-lr-decay --num-env-steps 10000000 --penetration-rate 1
